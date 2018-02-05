1d1fcc850a3628dd092a31136e53534c476fd099
rexfa <rex_fa@163.com>
2018-02-05 18:28:02 +0800

    建立，非常小的任务

---
+++ ShowMeYourIP/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.HttpOverrides;
+using ShowMeYourIP.Models;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace ShowMeYourIP.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IMemoryCache _memoryCache;
+        public HomeController(IMemoryCache memoryCache)
+        {
+            this._memoryCache = memoryCache;
+        }
+        public IActionResult Index()
+        {
+            var ip = Request.HttpContext.Connection.RemoteIpAddress;
+            try
+            {
+                string ipstring = ip.ToString();
+
+                ViewData["Remote-IP"] = ipstring;
+            }
+            catch (Exception ex)
+            {
+                ViewData["Remote-IP"] = ex.Message;
+            }
+            string 
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        #region function
+        //private string ShowIP()
+        //{
+        //    //string userIP;
+        //    //// HttpRequest Request = HttpContext.Current.Request;  
+        //    //HttpRequest Request = HttpContext.Current.Request.UserHostAddress;  // 如果使用代理，获取真实IP  
+        //    //if (Request.ServerVariables["HTTP_X_FORWARDED_FOR"] != "")
+        //    //    userIP = Request.ServerVariables["REMOTE_ADDR"];
+        //    //else
+        //    //    userIP = Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+        //    //if (userIP == null || userIP == "")
+        //    //    userIP = Request.UserHostAddress;
+        //    //return userIP;
+        //}
+        #endregion
+    }
+}
---
+++ ShowMeYourIP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ShowMeYourIP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
